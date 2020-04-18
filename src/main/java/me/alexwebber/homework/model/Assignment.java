6770ca4e4bb309e4dc848e1c45c553cd64b2a2e5
Alex Webber <webber512@live.missouristate.edu>
2020-04-18 13:15:46 -0500

    Added Assignment Interface

--- src/main/java/me/alexwebber/homework/model/Assignment.java
+++ src/main/java/me/alexwebber/homework/model/Assignment.java
@@ -6,6 +6,7 @@ public class Assignment {
 	private String name;
 	private String type;
 	private Integer classId;
+	private String description;
 	private String dueDate;
 	private String completedDate;
 	private String completed;
@@ -66,6 +67,14 @@ public class Assignment {
 		this.completed = completed;
 	}
 
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
 	@Override
 	public int hashCode() {
 		final int prime = 31;
@@ -73,6 +82,7 @@ public class Assignment {
 		result = prime * result + ((classId == null) ? 0 : classId.hashCode());
 		result = prime * result + ((completed == null) ? 0 : completed.hashCode());
 		result = prime * result + ((completedDate == null) ? 0 : completedDate.hashCode());
+		result = prime * result + ((description == null) ? 0 : description.hashCode());
 		result = prime * result + ((dueDate == null) ? 0 : dueDate.hashCode());
 		result = prime * result + ((id == null) ? 0 : id.hashCode());
 		result = prime * result + ((name == null) ? 0 : name.hashCode());
@@ -104,6 +114,11 @@ public class Assignment {
 				return false;
 		} else if (!completedDate.equals(other.completedDate))
 			return false;
+		if (description == null) {
+			if (other.description != null)
+				return false;
+		} else if (!description.equals(other.description))
+			return false;
 		if (dueDate == null) {
 			if (other.dueDate != null)
 				return false;
@@ -129,8 +144,9 @@ public class Assignment {
 
 	@Override
 	public String toString() {
-		return "Assignment [id=" + id + ", name=" + name + ", type=" + type + ", classId=" + classId + ", dueDate="
-				+ dueDate + ", completedDate=" + completedDate + ", completed=" + completed + "]";
+		return "Assignment [id=" + id + ", name=" + name + ", type=" + type + ", classId=" + classId + ", description="
+				+ description + ", dueDate=" + dueDate + ", completedDate=" + completedDate + ", completed=" + completed
+				+ "]";
 	}
 
 }
---
+++ src/main/java/me/alexwebber/homework/services/AssignmentService.java
@@ -0,0 +1,23 @@
+package me.alexwebber.homework.services;
+
+import java.util.List;
+
+import org.springframework.ui.Model;
+
+import me.alexwebber.homework.model.Assignment;
+
+public interface AssignmentService {
+
+	public List<Assignment> getAllAssignments();
+
+	public List<Assignment> getAssignmentsForClass(Integer id);
+
+	public Integer getNumberOfAssignments();
+
+	public Integer getNumberOfAssignmentsForClass();
+
+	public void addAssignment(Model model, Assignment assignment);
+
+	public void deleteAssignment(Integer id);
+
+}
