e6f5c67ef93ab4d606825c049ee39d767c896744
Alex Webber <webber512@live.missouristate.edu>
2020-04-19 11:13:58 -0500

    Added impl to mark assignments done

--- src/main/java/me/alexwebber/homework/controllers/MainController.java
+++ src/main/java/me/alexwebber/homework/controllers/MainController.java
@@ -9,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import me.alexwebber.homework.model.Assignment;
@@ -34,7 +35,7 @@ public class MainController {
 		double progress = (double) assignmentService.getNumberOfCompletedAssignments()
 				/ assignmentService.getNumberOfAssignments();
 		System.out.println(progress);
-		model.addAttribute("progress", progress*100);
+		model.addAttribute("progress", progress * 100);
 
 		model.addAttribute("classList", classService.getClasses());
 		model.addAttribute("command", new Assignment());
@@ -47,6 +48,12 @@ public class MainController {
 		model.addAttribute("classList", classService.getClasses());
 		assignmentService.addAssignment(model, assignment);
 		model.addAttribute("assignmentList", assignmentService.getOpenAssignments());
+		return "redirect:/home";
+	}
+
+	@GetMapping(value = { "/assignment/done/{id}" })
+	public String postCompleteAssignment(HttpSession session, Model model, @PathVariable Integer id) {
+		assignmentService.markAssignmentCompleted(id);
 		return "redirect:/home";
 	}
 
